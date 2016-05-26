ec2ebf5d0739e281293d6106b121f8cfd0f5c5ae
Cameron Palk <ckpalk@comcast.net>
2016-05-26 11:36:59 -0700

    Working on regex split for first line of course information

--- Web_Scrape/course.py
+++ Web_Scrape/course.py
@@ -1,61 +1,41 @@
 import re
+import metadata
 
 class Course( object ):
 
-	def __init__( self, rows ):
-		self.interpretRows( rows )
+	COURSE_KEYS = [ 'coursecode', 	'coursename', 	'credits',
+					'tag', 			'crn', 			'avail_seats', 	
+					'max_seats', 	'time', 		'day', 
+					'location', 	'instructor', 	'notes' ]
+
+	def __init__( self, _data_map ):
+		self.coursecode  = _data_map[ self.COURSE_KEYS[ 0] ]
+		self.coursename  = _data_map[ self.COURSE_KEYS[ 1] ]
+		self.credits 	 = _data_map[ self.COURSE_KEYS[ 2] ]
+		self.tag 		 = _data_map[ self.COURSE_KEYS[ 3] ]
+		self.crn 		 = _data_map[ self.COURSE_KEYS[ 4] ]
+		self.avail_seats = _data_map[ self.COURSE_KEYS[ 5] ]
+		self.max_seats 	 = _data_map[ self.COURSE_KEYS[ 6] ]
+		self.time 		 = _data_map[ self.COURSE_KEYS[ 7] ]
+		self.day 		 = _data_map[ self.COURSE_KEYS[ 8] ]
+		self.location 	 = _data_map[ self.COURSE_KEYS[ 9] ]
+		self.instructor  = _data_map[ self.COURSE_KEYS[10] ]
+		self.notes 		 = _data_map[ self.COURSE_KEYS[11] ]
+	#
 
 	''' BUILT-IN '''
 	def __repr__( self ):
-		return "{} : {:8} > {}".format( self.crn, self.subcode, self.classname )
+		return "{} : {:8} > {:24} by {}".format( 
+					self.crn, 
+					self.coursecode, 
+					self.classname,
+					self.instructor
+				)
 
 	''' HELPER '''
-	def interpretRows( self, rows ):
-		self.interpretRowHeader( rows[0] )
-		self.interpretRowData  ( rows[1] )
-	
-	def interpretRowHeader( self, row ):
-		# 'CTG 213 Intro Accounting II 4.00 cr.', 
-		words = row.split()
-		self.subcode   = ' '.join( words[  : 2] )
-		self.classname = ' '.join( words[ 2:-2] )
-		self.credits   = ' '.join( words[-2:  ] )
-		return
-		
-	def interpretRowData( self, row ):
-		# '10050 20 80 1200-1350 tr 220 HED Turner D AU'
-		# '+ Lab 10104 12 26 0800-0850 r 373 MCK tba A'
-		words = row.split()
-		print( words )
-		if re.match( '[1-9][0-9]{4}', words[ 0 ] ):
-			self.tag = None
-			self.crn = words[ 0 ]
-			next_idx = 1
-		else:
-			if words[0] is '+':
-				self.tag = ' '.join( words[ :1 ] )
-				self.crn = words[ 2 ]
-				next_idx = 3
-			else:
-				self.tag = words[ 0 ]
-				self.crn = words[ 1 ]
-				next_idx = 2
-		self.avail_seats = words[ next_idx 	   ]
-		self.max_seats   = words[ next_idx + 1 ]
-		self.time		 = words[ next_idx + 2 ]
-		self.days 		 = self.readDayString( \
-							words[ next_idx + 3 ] )
-		if re.match( '[0-9]+', words[ next_idx + 4 ] ):
-			self.location   = ' '.join( words[ next_idx + 4 : next_idx + 6 ] )
-			self.instructor = ' '.join( words[ next_idx + 6 : -1 ] )
-		else:
-			self.location   = words[ next_idx + 4 ]
-			self.instructor = ' '.join( words[ next_idx + 5 : -1 ] )
-		self.notes = words[ -1 ]
-		return
-
 	def readDayString( self, string ):
 		if string is 'tba': return None
+		else: return string
 		days = []
 		for day in list( string ):
 			if day is 'm': days.append( 'Monday' 	); continue
@@ -69,21 +49,4 @@ class Course( object ):
 
 
 if __name__ == '__main__':
-	c = Course( [ "CTG 213 Intro Accounting II 4.00 cr.", \
-				  "10050 20 80 1200-1350 tr 220 HED Turner D AU" ] )
-
-	c2 = Course( [ "PEAQ 351 Lifeguard Certificate 1.00 cr.", \
-				   "14733 13 23 1400-1520 t 10/25-11/22 87 SRC Vearrier R APV" ] )
-
-	print( c.subcode )
-	print( c.classname )
-	print( c.credits )
-	print( c.tag )
-	print( c.crn )
-	print( c.avail_seats )
-	print( c.max_seats )
-	print( c.time )
-	print( c.days )
-	print( c.location )
-	print( c.instructor )
-	print( c.notes )
+	print( "Nothing to run" )
--- Web_Scrape/scraper.py
+++ Web_Scrape/scraper.py
@@ -13,7 +13,23 @@ def isClassHeader( row ):
 #
 
 def getCRNRows( idx, rows ):
-	return [ ' '.join( rows[idx].text_content().split() ) for idx in [0,idx] ]
+	return [ rows[idx] for idx in [0,idx] ]
+#
+
+def cleanRows( rows ):
+	return [ [ ' '.join( cell.text_content().split() ) for cell in row ] \
+			for row in rows ]
+
+def rowsToDict( rows ):
+	rows = cleanRows( rows )
+	print( rows )
+	print( "String:", rows[0][0] )
+	vals = re.split( r'(?![0-9]{2,}) (?=[^0-9])', rows[0][0] )
+	print( vals )
+	for row in rows:
+		print( [ x.text_content() for x in row ] )
+	vals.extend( [ ' '.join( x.text_content().split() ) for x in row ] )
+	return { key : val for key,val in zip( Course.COURSE_KEYS, vals ) }
 #
 	
 def getClassMap( rows ):
@@ -25,9 +41,9 @@ def getClassMap( rows ):
 		if isClassHeader( row ) and class_rows:
 			for idx, crn in enumerate( getCRNS( class_rows ) ):
 				crn_rows = getCRNRows( idx + metadata.CRN_START_INDEX, class_rows )
-				if re.search( 'cancelled', crn_rows[1], re.IGNORECASE ):
+				if re.search( 'cancelled', crn_rows[1].text_content(), re.IGNORECASE ):
 					continue
-				Classes[ crn ] = Course( crn_rows )
+				Classes[ crn ] = Course( rowsToDict( crn_rows ) )
 			class_rows = []
 		class_rows.append( row )
 
@@ -66,7 +82,6 @@ def main( argv ):
 	rows = getTable( data )
 
 	Classes = getClassMap( rows )
-
 
 	for key, val in Classes.items():
 		print( val )
