e22d7f5c01bceffcf0cb23b4e99410b8b1b4aa5d
Cameron Palk <ckpalk@comcast.net>
2016-06-03 19:27:13 -0700

    Search is semi working, must convert search values before query

---
+++ RailsApp/app/controllers/search_controller.rb
@@ -0,0 +1,26 @@
+class SearchController < ApplicationController
+
+  def index
+  end
+
+  def results
+    @courses = Course.where cleanQueryHash
+    if @courses
+      # Redirect/render a courses not found page, possible redirect to :back
+      # Looking into notice or flash events which can pass a message between views
+      # Redirect to search and show that no courses were found
+      redirect_to( :back, alert: "No courses found" ) and return
+    end
+  end
+
+  private
+    # Build query hash
+    def cleanQueryHash
+      permitParams.select { |_,val| not val.empty?  }
+    end
+
+    def permitParams
+      params.permit(:coursecode, :coursename, :gsc, :credits, :tag, :crn, :avail_seats, :max_seats, :time, :day, :location, :notes, :instructor)
+    end
+
+end
--- RailsApp/config/routes.rb
+++ RailsApp/config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  resources :courses
+
+
+  # Search pages
+  get 'search/', to: 'search#index'
+  get 'search/results'
+
+  resources :courses, only: [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
